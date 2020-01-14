9830400ee5b116eaa99a1e1c86a6c58f4d0306a8
Aleexxeyy <asamosadov@gmail.com>
2020-01-15 01:49:25 +0300

    Подготовил для сдачи 6.2

--- src/controllers/board-controller.js
+++ src/controllers/board-controller.js
@@ -95,7 +95,6 @@ export default class BoardController extends AbstractComponent {
     this._showedTaskControllers.forEach((it) => it.setDefaultView());
   }
 
-
   _onDataChange(place, oldData, newData) {
     const index = this._renderingTasks.findIndex((it) => it === oldData);
 
@@ -103,7 +102,6 @@ export default class BoardController extends AbstractComponent {
     if (index === -1) {
       return;
     }
-
 
     this._renderingTasks = [].concat(this._renderingTasks.slice(0, index), newData, this._renderingTasks.slice(index + 1));
     place.render(this._renderingTasks[index]);
